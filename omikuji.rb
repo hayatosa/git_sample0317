579223dc942a47d921b0ac83f0ccb7bfd0bd1ac1
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2020-03-17 15:20:02 +0900

    sampleメソッドを使用

--- omikuji.rb
+++ omikuji.rb
@@ -1,17 +1,2 @@
-num = rand(8)
-
-if num == 0
-  puts "大吉"
-elsif num == 1
-  puts "中吉"
-elsif num == 2
-  puts "末吉"
-elsif num == 3
-  puts "凶"
-elsif num == 4
-  puts "末吉"
-elsif num == 5
-  puts "吉"
-else
-  puts "大凶"
-end
+omikuji = %w(大吉 中吉 小吉)
+puts omikuji.sample
